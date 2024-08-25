c68dc68bad751937329287297e1b04dc46c35c90
dinabecirovic <dinaabecirovic@gmail.com>
2024-08-25 11:01:14 +0200

    vehicle-form ts

--- src/app/vehicle-form/vehicle-form.component.ts
+++ src/app/vehicle-form/vehicle-form.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-vehicle-form',
   templateUrl: './vehicle-form.component.html',
   styleUrls: ['./vehicle-form.component.css']
 })
-export class VehicleFormComponent {
+export class VehicleFormComponent implements OnInit {
+  vehicleForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.vehicleForm = this.fb.group({
+      mark: ['', Validators.required],
+      name: ['', Validators.required],
+      regNumber: ['', [Validators.required, Validators.pattern('[A-Za-z0-9]+')]],
+    });
+  }
+
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    if (this.vehicleForm.valid) {
+      console.log('Vehicle Data:', this.vehicleForm.value);
+    }
+  }
 }
